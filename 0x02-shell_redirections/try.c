f1d90805f951747d23ddd7949b5237a1cb740bec
Igbobi williams <dabs.collection@gmail.com>
2023-02-18 07:27:22 -0800

    0-helloworld fix

---
+++ 0x02-shell_redirections/try.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: 'first program'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("There once was a man named\n");
+	return (0);
+}
